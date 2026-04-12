01500c45fa4f2c034fc4c2d1a516d20007a33e8a
Albert B. <albertjosep.bonet@students.salle.url.edu>
2026-04-12 23:48:40 +0200

    LoginGUI connected with registerGUI

--- src/Presentation/LoginForm.java
+++ src/Presentation/LoginForm.java
@@ -3,11 +3,14 @@ package Presentation;
 import javax.swing.*;
 import java.awt.*;
 
-
 public class LoginForm extends JPanel {
 
     public LoginForm(MainWindow mainWindow, AuthController auth, AuthPanel authPanel) {
-        setLayout(new GridLayout(12,1,5,5));
+
+        setLayout(new GridBagLayout());
+
+        JPanel inner = new JPanel(new GridLayout(0, 1, 5, 5));
+        inner.setPreferredSize(new Dimension(300, 220));
 
         JTextField email = new JTextField(20);
         JPasswordField password = new JPasswordField(20);
@@ -18,15 +21,15 @@ public class LoginForm extends JPanel {
         });
 
         JButton goToRegister = new JButton("Register if you do not have an account here");
-        //goToRegister.addActionListener(e -> {})
+        goToRegister.addActionListener(e -> authPanel.showRegister(mainWindow, auth));
 
-        add(new JLabel("Email"));
-        add(email);
-        add(new JLabel("Password"));
-        add (password);
-        add(loginButton);
-        add(goToRegister);
+        inner.add(new JLabel("Email"));
+        inner.add(email);
+        inner.add(new JLabel("Password"));
+        inner.add(password);
+        inner.add(loginButton);
+        inner.add(goToRegister);
 
+        add(inner);
     }
-
 }
