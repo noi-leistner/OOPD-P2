23ea896b35cffa323504ceee7fbc5870888e8aa2
Albert B. <albertjosep.bonet@students.salle.url.edu>
2026-04-15 08:22:45 +0200

    Added personalized messages for queries in AuthResult

--- src/Business/AuthResult.java
+++ src/Business/AuthResult.java
@@ -1,7 +1,30 @@
 package Business;
 
 public enum AuthResult {
-    SUCCESS,
-    EMAIL_ALREADY_EXISTS,
-    DATABASE_ERROR
+    SUCCESS,                            /*Successful log in */
+    INVALID_CREDENTIALS,                /*Either email or password wrong */
+    ACCOUNT_INACTIVE,                   /*Account deactivated */
+    EMPTY_FIELDS,                       /*1 or more field that are null */
+    EMAIL_ALREADY_EXISTS,               /*Email already created when registering */
+    DATABASE_ERROR;                     /*Error in the database*/
+
+    /**
+     * Translates enumeration into a readable message:
+     */
+
+    public String toDisplayMessage () {
+        return switch (this) {
+            case SUCCESS -> "Logout Successful";
+            case INVALID_CREDENTIALS -> "Invalid Credentials";
+            case ACCOUNT_INACTIVE -> "Account Inactive";
+            case EMPTY_FIELDS -> "Empty Fields";
+            case EMAIL_ALREADY_EXISTS -> "Email Already Exists";
+            case DATABASE_ERROR -> "Database Error";
+        };
+    }
+
+    public boolean isSuccess () {return this == SUCCESS;}
+
 }
+
+
