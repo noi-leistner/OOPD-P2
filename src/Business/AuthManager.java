9b7b94bff4ff369fc62a7dc1dfb69ce7739b20bd
Albert B. <albertjosep.bonet@students.salle.url.edu>
2026-04-28 21:44:07 +0200

    Added hash password

--- src/Business/AuthManager.java
+++ src/Business/AuthManager.java
@@ -2,6 +2,7 @@ package Business;
 
 import Business.Entities.User;
 import Persistance.UserDAOSql;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class AuthManager {
 
@@ -12,35 +13,27 @@ public class AuthManager {
     }
 
     public AuthResult login(String email, String password) {
-        // 1. Validación de campos (Muy bien hecho)
         if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
             return AuthResult.EMPTY_FIELDS;
         }
-
-        // 2. Intento de recuperar usuario
         User user = userDAO.getUserByEmail(email.trim());
 
-        // Si el DAO devuelve null (porque no existe o por error de SQL)
         if (user == null) {
             System.out.println("DEBUG: EMAIL NOT FOUND");
             return AuthResult.INVALID_CREDENTIALS;
         }
 
-        // 3. Verificación de contraseña (texto plano de momento)
-        if (!password.equals(user.getPassword())) {
-            System.out.println("DEBUG: USER PASSWORD NOT FOUND");
-            return AuthResult.INVALID_CREDENTIALS;
-        } else {
-            System.out.println("USER FOUND");
-            // 4. ÉXITO: Guardamos en la sesión ANTES de devolver el resultado
+        if (BCrypt.checkpw(password, user.getPassword())) {
             SessionManager.getInstance().login(user);
-            System.out.println("✅ Sesión iniciada para: " + user.getEmail());
             return AuthResult.SUCCESS;
         }
+        return AuthResult.INVALID_CREDENTIALS;
     }
 
     public AuthResult signUp(User user) {
         if (user == null) return AuthResult.DATABASE_ERROR;
+        String hashedPass = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashedPass);
         return userDAO.addUser(user);
     }
 
--- src/Business/Entities/User.java
+++ src/Business/Entities/User.java
@@ -32,4 +32,5 @@ public class User {
     public String getPassword() { return password; }
     public String getRole()    { return role; }
     public void setRole(String role) { this.role = role; }
+    public void setPassword(String password) { this.password = password; }
 }
