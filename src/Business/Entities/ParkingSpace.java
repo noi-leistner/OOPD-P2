6f3518aa221accc1c1b112986d473fd597b91e61
Albert B. <albertjosep.bonet@students.salle.url.edu>
2026-05-04 20:39:06 +0200

    Basic functions for checking the parking_spots table implemented and working.

--- src/Business/Entities/ParkingSpace.java
+++ src/Business/Entities/ParkingSpace.java
@@ -3,8 +3,8 @@ package Business.Entities;
 public class ParkingSpace {
         int code;
         int floor;
-        String current_status;
-        String reservation_status;
+        int current_status;
+        int reservation_status;
         String type;
         int spaceId;
 
@@ -16,20 +16,30 @@ public class ParkingSpace {
                 return floor;
         }
 
-        public String getCurrentStatus() {
+        public int getCurrentStatus() {
                 return current_status;
         }
 
-        public String getReservationStatus() {
+        public int getReservationStatus() {
                 return reservation_status;
         }
 
         public String getType() {
                 return type;
         }
+        public int getSpaceId() {return spaceId;}
 
-        public int getSpaceId() {
-                return spaceId;
-        }
+        public void setSpaceId(int spaceId) {this.spaceId = spaceId;}
+
+        public void setCode(int code) {this.code = code;}
+
+        public void setFloor(int floor) {this.floor = floor;}
+
+        public void setCurrentStatus(int current_status) {this.current_status = current_status;}
+
+        public void setReservationStatus(int reservation_status) {this.reservation_status = reservation_status;}
+
+        public void setType(String type) {this.type = type;}
 }
+
 
