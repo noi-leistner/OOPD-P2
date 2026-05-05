141465f73af9ffd2239a23b2584841860f3d6589
Albert B. <albertjosep.bonet@students.salle.url.edu>
2026-05-05 13:03:31 +0200

    Added necessary getters and setters for Reservations.j and Vehicle.j

--- src/Business/Entities/Reservation.java
+++ src/Business/Entities/Reservation.java
@@ -3,7 +3,29 @@ package Business.Entities;
 import java.util.Date;
 
 public class Reservation {
-    String license_plate;
-    int parking_code;
+    int id;                         // DB: id (auto-increment)
+    int user_id;
+    String vehicle_license_plate;
+    int parking_slot_id;
     Date date;
+
+    public int getId() {return id;}
+
+    public int getUser_id() {return user_id;}
+
+    public String getVehicle_license_plate() {return vehicle_license_plate;}
+
+    public int getParking_slot_id() {return parking_slot_id;}
+
+    public Date getDate() {return date;}
+
+    public void setId(int id) {this.id = id;}
+
+    public void setUser_id(int user_id) {this.user_id = user_id;}
+
+    public void setVehicle_license_plate(String vehicle_license_plate) {this.vehicle_license_plate = vehicle_license_plate;}
+
+    public void setParking_slot_id(int parking_slot_id) {this.parking_slot_id = parking_slot_id;}
+
+    public void setDate(Date date) {this.date = date;}
 }
--- src/Business/Entities/Vehicle.java
+++ src/Business/Entities/Vehicle.java
@@ -4,4 +4,10 @@ public class Vehicle {
     String license_plate;
     String type;
 
+    public String getLicense_plate() {return license_plate;}
+    public String getType() {return type;}
+
+    public void setLicense_plate(String license_plate) {this.license_plate = license_plate;}
+
+    public void setType(String type) {this.type = type;}
 }
