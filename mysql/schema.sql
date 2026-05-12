4f142bf46f4f9d75308b0262db44e61ba1b83210
pabs <paugomezthau1@gmail.com>
2026-05-12 16:23:52 +0200

    schema file

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -20,7 +20,9 @@ CREATE TABLE parking_slots (
     floor      INT NOT NULL,
     vehicle_type VARCHAR(30) NOT NULL,
     occupation_status BOOLEAN NOT NULL DEFAULT FALSE,
-    reservation_status BOOLEAN NOT NULL DEFAULT FALSE
+    reservation_status BOOLEAN NOT NULL DEFAULT FALSE,
+    parked_vehicle_plate VARCHAR(20),
+    FOREIGN KEY (parked_vehicle_plate) REFERENCES vehicles(license_plate)
 );
 
 -- Create reservations table (ternary relationship: User 1 - Vehicle 1 - Parking Slot N)
@@ -44,8 +46,8 @@ INSERT INTO vehicles (license_plate, vehicle_type) VALUES
 ('1234ABC', 'car'),
 ('5678DEF', 'motorcycle');
 
-INSERT INTO parking_slots (identifier, vehicle_type, occupation_status, reservation_status, floor) VALUES
-(1, 'car', FALSE, FALSE, 0),
-(2, 'car', FALSE, FALSE, 1),
-(3, 'motorcycle', FALSE, FALSE, 0),
-(4, 'motorcycle', FALSE, FALSE, 1);
+INSERT INTO parking_slots (identifier, vehicle_type, occupation_status, reservation_status, floor, parked_vehicle_plate) VALUES
+(1, 'car', FALSE, FALSE, 0, NULL),
+(2, 'car', FALSE, FALSE, 1, NULL),
+(3, 'motorcycle', FALSE, FALSE, 0, NULL),
+(4, 'motorcycle', FALSE, FALSE, 1, NULL);
